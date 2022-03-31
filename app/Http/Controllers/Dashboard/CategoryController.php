9653e14d45323749437476e54c64a79ae060157e
Alex <alexanderjobss@gmail.com>
2022-03-31 19:11:24 +0200

    Categorias CRUD

---
+++ app/Http/Controllers/Dashboard/CategoryController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Models\Post;
+use App\Models\Category;
+use Illuminate\Support\Str;
+
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Category\PutRequest;
+use App\Http\Requests\Category\StoreRequest;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = Category::paginate(3);
+        return view('dashboard.category.index', compact('categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {   
+        $category = new Category();
+        return view('dashboard.category.create',compact('category'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreRequest $request)
+    {
+        $data = $request->all();
+        Category::create($data);
+
+        return to_route('category.index')->with('status', 'Registro creado');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        return view("dashboard.category.show", compact('category'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {       
+        return view('dashboard.category.edit', compact('category'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(PutRequest $request, Category $category)
+    {       
+        $category->update($request->validated());
+        return to_route('category.index')->with('status', 'Registro actualizado');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return to_route('category.index')->with('status', 'Registro eliminado');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
-use App\Http\Controllers\Dashboard\PostController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Dashboard\PostController;
+use App\Http\Controllers\Dashboard\CategoryController;
 
 Route::resource('post', PostController::class);
+Route::resource('category', CategoryController::class);
